1fb788d0030c8b8cc5862c833f8e3b11f704ca2d
John Roll <john@cfa.harvard.edu>
2014-03-28 06:18:16 -0400

    Working

--- ReplaceIdRefs.js
+++ ReplaceIdRefs.js
@@ -1,36 +1,55 @@
+/*jslint white: true, vars: true, plusplus: true, nomen: true, unparam: true, evil: true, regexp: true */
+/*jshint node: true, -W099: true, laxbreak:true, laxcomma:true, multistr:true, smarttabs:true */
 
-function ReplaceIdRefs(str, func) 
-    reply = "";
+"use strict";
 
-    var i. state = -1;
+function replaceIdRefs(str, func) {
+    var reply = "";
 
+    var state = -1, match, index, first, i = 0;
+
+console.log(str, str.length)
     while ( i < str.length ) {
 	match = str.match(/[a-zA-Z_][a-zA-Z0-9_]*/);		// Find an identifier in the string.
-	ident = match;
+
+	if ( !match ) { break; }
+
 	index = [];
 	i     = match.index + match[0].length;
 
-	state = 1;
+	state = 0;
 
-	while ( state ) {
-	    if ( state === 1 ) {
-		while ( str[i] === ' ' ) { index++ };	// Skip blanks
+	while ( i < str.length ) {
+	    if ( str[i] === ']' ) {
+		state--;
 
-		if ( str[i] != '[' ) { break; }
+		if ( state === 0 ) {
+		    index.push(str.substring(first, i)); 
+		}
+	    } else {
+		if ( state === 0 ) {
+		    if ( str[i] === '[' ) {
+			first = i+1;
+			state++;
+		    }
+		    if ( str[i] !== ' ' && str[i] !== '[' ) {
+			break;
+		    }
+		} else {
+		    if ( str[i] === '[' ) { state++; }
+		}
 	    }
-
-	    if ( str[index === '[' ) {				// push array of indicies
-		first = i;
-		state = 0;
-
-		if ( str[i] === "[" ) { if ( state++ === 0 ) { i = i+1; } }
-		if ( str[i] === "]" ) { if ( state-- === 1 ) { index.push(str.substring(first, i)); } }
-	    }
+	    i++
 	}
 
 	reply += str.substr(0, match.index) + func(match[0], index);
-	str    = str.substr(index);
+	str    = str.substr(i);
+	i = 0;
     }
 
-    return reply;
+    return reply + str.substr(i);
 }
+
+console.log(replaceIdRefs("{ hhh + www[1  ][2][3]", function (x, y) { return ":" + x + y + ":"; }));
+console.log(replaceIdRefs("www[1 + T[1]]  [1234] }", function (x, y) { return ":" + x + y + ":"; }));
+
