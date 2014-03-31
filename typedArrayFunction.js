8b0d2c31c4024f3102c8e0ff9d29f267fdc2c8bf
John Roll <john@cfa.harvard.edu>
2014-03-30 21:22:56 -0400

    begin numeric support

--- typedArrayFunction.js
+++ typedArrayFunction.js
@@ -5,47 +5,56 @@
 "use strict";
 
 (function() {
+    var numeric = {};
+
+    numeric._dim = function _dim(x) {
+	var ret = [];
+	while(typeof x === "object") { ret.push(x.length); x = x[0]; }
+	return ret;
+    };
 
     function replaceIdentifierRefs(str, func) {
 	var reply = "";
 
-	var state = -1, match, index, first, i = 0;
+	var state = -1, match, index, first, i = 0, x;
 
 	while ( i < str.length ) {
 	    match = str.match(/[a-zA-Z_][a-zA-Z0-9_]*/);		// Find an identifier in the string.
 
 	    if ( !match ) { break; }
 
-	    reply += str.substr(i, match.index)
+	    reply += str.substr(i, match.index);
 
 	    index = [];
 	    i     = match.index + match[0].length;
 
-	    var x = true;
+	    x = true;
 	    while ( x && i < str.length ) {
-		while ( str[i] == ' ' ) { i++; }
+		while ( str[i] === ' ' ) { i++; }
+
 		switch ( str[i] ) {
-		 case "[": {
+		 case "[": 
 		    state = 1;
 		    first = i+1;
 		    i++;
 
 		    while ( state ) {
-			if ( str[i] == ']' ) {
-			    if ( state == 1 ) index.push(str.substring(first, i)); 
+			if ( str[i] === ']' ) {
+			    if ( state === 1 ) {
+				index.push(str.substring(first, i)); 
+			    }
 			    state--;
 			}
-			if ( str[i] == '[' ) {
+			if ( str[i] === '[' ) {
 			    state++;
 			    first = i+1;
 			}
 			i++;
 		    }
-		 }
-		 default: {
+		    break;
+		 default: 
 		    x = false;
 		    break;
-		 }
 		}
 	    }
 
@@ -60,13 +69,20 @@
 
 
     function typedArrayFunctionConstructor() {
-        var i;
+        var i, text;
         var actuals = arguments;
 	var hash = {};
+	var star = [];
 	var dims = [];
 	var indicies = [ "iW", "iV", "iU", "iZ", "iY", "iX" ];
 
-	var text = this.func.toString();
+	if ( typeof this.func === "string" ) {
+	    text = this.func;
+	} else {
+	    text = this.func.toString();
+	}
+
+	var consider = this.consider;
 
 	var x = text.match(/function \(([^()]*)\)[^{]*{([^]*)}[^]*/);
 
@@ -86,66 +102,95 @@
 	// Capture the function parameter names and place them in the 
 	// hash table with corrosponding real function arguments.
 	//
-	var type = ""
+	var type = "";
 	for ( i = 0; i < args.length; i++ ) {
 	    hash[args[i]] = actuals[i];
-	    type += "// var " + args[i] + " " + actuals[i].dtype + " " + actuals[i].offset + " " + actuals[i].shape + " " + actuals[i].stride + "\n"
+
+	    if ( typeof actuals[i] === "object" && !actuals[i].shape && (!consider || consider && consider[args[i]] ) ) {
+		actuals[i].shape = numeric._dim(actuals[i]);
+	    }
+	    type += "// var " + args[i] + " " + actuals[i].dtype + " " + actuals[i].offset + " " + actuals[i].shape + " " + actuals[i].stride + "\n";
        	}
 
 	// Match each source code identifier and any associated array indexing.  Extract
 	// the indicies and recursivly replace them also.
 	//
-	function ReplaceArrayRefs(text) {
+	function replaceArrayRefs(text) {
 	    return replaceIdentifierRefs(text, function (id, indx) {
-		var i, state = 0, offset;
+		var i, offset, reply;
 
 		for ( i = 0; i < indx.length; i++ ) {
-		    indx[i] = ReplaceArrayRefs(indx[i]);
+		    indx[i] = replaceArrayRefs(indx[i]);
 		}
 
 
 		var arg = hash[id];
+		var dimen;
+		var joinStr;
 
-		if ( arg && arg.data ) {
+		if ( arg && typeof arg === "object" && (!consider || ( consider && consider[args[i]] )) ) {
 
-		    indi = indicies.slice(6-arg.dimension);
+		    if ( arg.data ) {
+			dimen = arg.dimension;
 
-		    id = id + ".data";
-		    for ( i = 0; i < arg.dimension; i++ ) {
+			id = id + ".data";
+			for ( i = 0; i < arg.dimension; i++ ) {
+			    if ( arg.stride[i] !== 1 ) { indx[i] =  "(" + indx[i] + ")*" + arg.stride[i]; }
+			}
+			if ( arg.offset !== 0 ) { 	offset = arg.offset + " + "; 
+			} else {			offset = ""; }
+
+			joinStr = " + ";
+		    } else {
+			dimen = arg.shape.length;
+			joinStr = "][";
+		    }
+
+		    var indi = indicies.slice(6-dimen);
+
+		    for ( i = 0; i < dimen; i++ ) {
 		    	if ( indx[i] === undefined ) { indx[i] = indi[i]; } 
 			if ( dims[i] === undefined ) { dims[i] = 0; }
 
 			dims[i] = Math.max(dims[i], arg.shape[i]);
 		    }
-		    for ( i = 0; i < arg.dimension; i++ ) {
-			if ( arg.stride[i] !== 1 ) { indx[i] =  "(" + indx[i] + ")*" + arg.stride[i]; }
-		    }
 
-		    if ( arg.offset !== 0 ) { 	offset = arg.offset + " + "; 
-		    } else {			offset = "" }
-
-		    return id + "[" + offset + indx.join(" + ") + "] ";
+		    reply = id + "[" + offset + indx.join(joinStr) + "] ";
 		} else {
 		    if ( indx.length > 0 ) {
-			return id + "[" + indx.join(" + ") + "] ";
+			reply = id + "[" + indx.join("][") + "] ";
 		    } else {
-			return id + " ";
+			reply = id + " ";
 		    }
 		}
+		
+		return reply;
 	    });
 	}
-	body = ReplaceArrayRefs(body);
+	var brak = body.match(/\/\/ *\[(.*)\]/);
+
+	body = replaceArrayRefs(body);
+	star = dims.map(function (x) { return 0; });
+
+	if ( brak !== null ) {
+	    brak = brak[1].replace(/\]\[/, " ").split(" ").map(function (x) { return x.split(":").map(function (n) { return parseInt(n, 10); }); });
+	    for ( i = 0; i < brak.length; i++ ) {
+		star[i] = brak[i][0];
+		dims[i] = brak[i][1] + dims[i];
+	    }
+	}
 
 	var indi = indicies.slice(6-dims.length).reverse();
+	dims.reverse();
 
 	for ( i = 0; i < dims.length; i++ ) {
-	    body = "for ( var " + indi[i] + " = 0; " + indi[i] + " < " + dims[i] + "; " + indi[i] + "++ ) {\n    " + body + "\n    }";
+	    body = "for ( var " + indi[i] + " = " + star[i] + "; " + indi[i] + " < " + dims[i] + "; " + indi[i] + "++ ) {\n    " + body + "\n    }";
 	}
 
 	var func;
 
 	func  = "// Array optimized funciton\n";
-	func += type
+	func += type;
 	func += "return function (" + args.join(",") + ") {\n'use strict';\n\n" + prep + body + post + "\n}";
 
 	if ( this.cache       === undefined ) { this.cache = {}; }
@@ -157,6 +202,6 @@
 	return this.cache[func].apply(undefined, arguments);
     }
 
-    module.exports = function (func) { return typedArrayFunctionConstructor.bind({ func: func }); };
+    module.exports = function (func, opts) { return typedArrayFunctionConstructor.bind({ func: func, opts: opts }); };
 }());
 
