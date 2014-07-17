4e4718cbb59f8e137a9ba2c1122f5e49d5ef0a35
John Roll <john@cfa.harvard.edu>
2014-07-17 08:25:20 -0400

    publish typed-array-function

--- npm/typed-array-function/typed-array-function.js
+++ npm/typed-array-function/typed-array-function.js
@@ -166,7 +166,7 @@
 	var func;
 
 	for ( i = 0; i < args.length; i++ ) {
-	    if ( actuals[i] !== undefined && typeof actuals[i] === "object"
+	    if ( actuals[i] !== null && actuals[i] !== undefined && typeof actuals[i] === "object"
 	     && (opts.consider === undefined || ( typeof opts.consider === "object" && opts.consider[args[i]] !== false )) ) {
 
 		hash[args[i]] = actuals[i];
@@ -354,21 +354,23 @@
 	func += "// " + type + "\n";
 	func += "return function (" + args.join(",") + ") {\n'use strict';\n\n" + init + prep + setp + body + post + "\n}";
 
-	if ( this.cache       === undefined ) { this.cache = {}; }
-	if ( this.cache[type] === undefined ) {
-	     if ( typed.debug ) { console.log(func); }
-	     func = new Function(func)();
-	     this.cache[type] = func;
-	}
+	if ( typed.debug ) { console.log(func); }
+
+	if ( this.cache === undefined ) { this.cache = {}; }
+
+	func = new Function(func)();
+	this.cache[type] = func;
 
 	return func;
     }
 
 
     function typedArrayFunctionExecute() {
-	var func = typedArrayFunctionConstructor.apply(this, arguments);
+	var func  = typedArrayFunctionConstructor.apply(this, arguments);
 
-	return func.apply(typed, arguments);
+	var reply = func.apply(typed, arguments);
+
+	return reply;
     }
 
     function typed(opts, func) {
